8b23db8970d38cb8edd75b777535ac7ae135c94d
lecourtier.frederique <lecourtier.frederique@gmail.com>
2022-05-21 17:06:58 +0200

    création de fonctions pour choisir si on veut créer les fichiers csv

--- para_real_method/para-real.py
+++ para_real_method/para-real.py
@@ -1,7 +1,6 @@
 import numpy as np
 import os
 import pandas as panda
-import mpi4py
 
 #### TO CLEAN ####
 
--- para_real_method/para-real_parallele.py
+++ para_real_method/para-real_parallele.py
@@ -89,8 +89,69 @@ def compute_time(t0,T,dt_G):
 def sol_converge(x0_k,x0_knext,eps=1e-6):
     return (np.max(np.abs(x0_knext-x0_k)) < eps)
 
+#to create and write in the csv files
+def csv_files(t0,T,dt_F,times,k,solution,init_pts,reshape_size,fct,gamma=None):
+    # time between t0 and T for the fine integrator ##
+    t = np.arange(t0,T+1e-6,dt_F)
+
+    # init
+    init_pts_x = panda.DataFrame(times[:-1],columns=['t'],dtype=np.float64)
+    init_pts_y = panda.DataFrame(times[:-1],columns=['t'],dtype=np.float64)
+    init_pts_z = panda.DataFrame(times[:-1],columns=['t'],dtype=np.float64)
+
+    solutions_x = panda.DataFrame(t,columns=['t'],dtype=np.float64)
+    solutions_y = panda.DataFrame(t,columns=['t'],dtype=np.float64)
+    solutions_z = panda.DataFrame(t,columns=['t'],dtype=np.float64)
+
+    # write
+    for k in range(k):
+        X0_k = init_pts[k,:]
+        init_pts_x.insert(len(init_pts_x.columns),'k='+str(k),X0_k[:,0])
+        init_pts_y.insert(len(init_pts_y.columns),'k='+str(k),X0_k[:,1])
+        init_pts_z.insert(len(init_pts_z.columns),'k='+str(k),X0_k[:,2])
+
+        sol_k = solution[k,:]
+        sol_k = sol_k.reshape((-1,reshape_size))
+        solutions_x.insert(len(solutions_x.columns),'k='+str(k),sol_k[:,0])
+        solutions_y.insert(len(solutions_y.columns),'k='+str(k),sol_k[:,1])
+        solutions_z.insert(len(solutions_z.columns),'k='+str(k),sol_k[:,2])
+
+    # to convert dataframe to csv files
+    init_pts_x.to_csv('donnees_para_real/init_pt_x.csv')
+    init_pts_y.to_csv('donnees_para_real/init_pt_y.csv')
+    init_pts_z.to_csv('donnees_para_real/init_pt_z.csv')
+    solutions_x.to_csv('donnees_para_real/solx.csv')
+    solutions_y.to_csv('donnees_para_real/soly.csv')
+    solutions_z.to_csv('donnees_para_real/solz.csv')
+
+    # avec RK4
+    sol = RK4(init_pts[0,:][0],dt_F,t0,T,fct,gamma)
+
+    sol_rk4 = panda.DataFrame(t,columns=['t'],dtype=np.float64)
+    sol_rk4.insert(len(sol_rk4.columns),'x',sol[:,0])
+    sol_rk4.insert(len(sol_rk4.columns),'y',sol[:,1])
+    sol_rk4.insert(len(sol_rk4.columns),'z',sol[:,2])
+    sol_rk4.to_csv('donnees_para_real/sol_rk4.csv')
+
+    print("Fichiers csv créés")
+
+def compute_sol_k(fin,X0_k_j):
+    sol_k_j = fin[1:].flatten()
+    if(rank==0):
+        temp = np.array([X0_k_j])
+        temp = np.append(temp,sol_k_j)
+        sol_k_j=temp
+    nb_tj = np.array(comm.gather(len(sol_k_j), 0))
+    sol_k=None
+    if(rank==0):
+        sol_k = np.empty(sum(nb_tj))
+    comm.Gatherv(sendbuf=sol_k_j, recvbuf=(sol_k, nb_tj), root=0)
+
+    return sol_k
+
+
 # P = number of processing units ; j in {0,...,P} 
-def parareal_method(X0_t0,t0,T,fct,dt_G,dt_F,gamma=None): 
+def parareal_method(X0_t0,t0,T,fct,dt_G,dt_F,gamma=None,write_csv=True): 
     # fine integrator
     def F(initial_point,t_j1,t_j2):
         return RK4(initial_point,dt_F,t_j1,t_j2,fct,gamma) 
@@ -102,24 +163,12 @@ def parareal_method(X0_t0,t0,T,fct,dt_G,dt_F,gamma=None):
 
     times = []
     X0_k = np.empty((P,len(X0_t0))) 
-    sol_k = np.array([])
+    
     grossier_k = np.empty((P-1,len(X0_t0))) #pas de valeur pour j=0
 
-    if(rank == 0):    
-        ## time between t0 and T for the fine integrator ##
-        t = np.arange(t0,T+1e-6,dt_F)
-        # nb_t = len(t)
-
+    if(rank == 0):   
         ## we initialize the time intervals for each processing units ##
         times=compute_time(t0,T,dt_G)
-        print("times :",times)
-
-        solutions_x = panda.DataFrame(t,columns=['t'],dtype=np.float64)
-        solutions_y = panda.DataFrame(t,columns=['t'],dtype=np.float64)
-        solutions_z = panda.DataFrame(t,columns=['t'],dtype=np.float64)
-        init_pts_x = panda.DataFrame(times[:-1],columns=['t'],dtype=np.float64)
-        init_pts_y = panda.DataFrame(times[:-1],columns=['t'],dtype=np.float64)
-        init_pts_z = panda.DataFrame(times[:-1],columns=['t'],dtype=np.float64)
 
         #### Itération k=0 ####
 
@@ -132,10 +181,6 @@ def parareal_method(X0_t0,t0,T,fct,dt_G,dt_F,gamma=None):
             X0_k[j] = X0_j
             comm.send(X0_k[j], dest=j, tag=j)   
             grossier_k[j-1] = X0_j         
-        
-        init_pts_x.insert(len(init_pts_x.columns),'k=0',X0_k[:,0])
-        init_pts_y.insert(len(init_pts_y.columns),'k=0',X0_k[:,1])
-        init_pts_z.insert(len(init_pts_z.columns),'k=0',X0_k[:,2])
 
     if(rank>0):
         X0_k_j = comm.recv(source=0, tag=rank)
@@ -150,23 +195,11 @@ def parareal_method(X0_t0,t0,T,fct,dt_G,dt_F,gamma=None):
     fin_k_j = fin[-1]
     fin_k = np.array(comm.gather(fin_k_j, 0))
 
-    sol_k_j = fin[1:].flatten()
-    if(rank==0):
-        temp = np.array([X0_k_j])
-        temp = np.append(temp,sol_k_j)
-        sol_k_j=temp
-    nb_tj = np.array(comm.gather(len(sol_k_j), 0))
-    sol_k=None
-    if(rank==0):
-        sol_k = np.empty(sum(nb_tj))
-    comm.Gatherv(sendbuf=sol_k_j, recvbuf=(sol_k, nb_tj), root=0)
-    if(rank==0):
-        sol_k = sol_k.reshape((-1,len(X0_t0)))
-        solutions_x.insert(len(solutions_x.columns),'k=0',sol_k[:,0])
-        solutions_y.insert(len(solutions_y.columns),'k=0',sol_k[:,1])
-        solutions_z.insert(len(solutions_z.columns),'k=0',sol_k[:,2])
-
-
+    if(write_csv):
+        sol_k = compute_sol_k(fin,X0_k_j)
+        if(rank==0):
+            solution = [sol_k]
+            init_pts = [X0_k]
 
     #### Itérations suivantes (jusqu'à ce que la solution converge) ####
     k=1
@@ -186,8 +219,6 @@ def parareal_method(X0_t0,t0,T,fct,dt_G,dt_F,gamma=None):
                 comm.send(X0_k[j], dest=j, tag=j)
                 grossier_k[j-1] = grossier_k_j
 
-            print(X0_kp)
-
         if(rank>0):
             X0_k_j = comm.recv(source=0, tag=rank)
 
@@ -196,26 +227,11 @@ def parareal_method(X0_t0,t0,T,fct,dt_G,dt_F,gamma=None):
         fin_k_j = fin[-1]
         fin_k = np.array(comm.gather(fin_k_j, 0))
 
-        sol_k_j = fin[1:].flatten()
-        if(rank==0):
-            temp = np.array([X0_k_j])
-            temp = np.append(temp,sol_k_j)
-            sol_k_j=temp
-        sol_k=None
-        if(rank==0):
-            sol_k = np.empty(sum(nb_tj))
-        comm.Gatherv(sendbuf=sol_k_j, recvbuf=(sol_k, nb_tj), root=0)
-        
-        if(rank==0):
-            sol_k = sol_k.reshape((-1,len(X0_t0)))
-
-            init_pts_x.insert(len(init_pts_x.columns),'k='+str(k),X0_kp[:,0])
-            init_pts_y.insert(len(init_pts_y.columns),'k='+str(k),X0_kp[:,1])
-            init_pts_z.insert(len(init_pts_z.columns),'k='+str(k),X0_kp[:,2])
-
-            solutions_x.insert(len(solutions_x.columns),'k='+str(k),sol_k[:,0])
-            solutions_y.insert(len(solutions_y.columns),'k='+str(k),sol_k[:,1])
-            solutions_z.insert(len(solutions_z.columns),'k='+str(k),sol_k[:,2])
+        if(write_csv):
+            sol_k = compute_sol_k(fin,X0_k_j)
+            if(rank==0):
+                solution = np.concatenate((solution,[sol_k]),axis=0)
+                init_pts = np.concatenate((init_pts,[X0_kp]),axis=0)
 
         if(rank==0):
             converge = sol_converge(X0_k,X0_kp)
@@ -228,13 +244,7 @@ def parareal_method(X0_t0,t0,T,fct,dt_G,dt_F,gamma=None):
         k+=1
 
     if(rank==0):
-        init_pts_x.to_csv('donnees_para_real/init_pt_x.csv')
-        init_pts_y.to_csv('donnees_para_real/init_pt_y.csv')
-        init_pts_z.to_csv('donnees_para_real/init_pt_z.csv')
-        solutions_x.to_csv('donnees_para_real/solx.csv')
-        solutions_y.to_csv('donnees_para_real/soly.csv')
-        solutions_z.to_csv('donnees_para_real/solz.csv')
-
+        csv_files(t0,T,dt_F,times,k,solution,init_pts,len(X0_t0),fct,gamma)
         print(k," itérations")
 
     MPI.Finalize()
@@ -245,19 +255,7 @@ X0=[5.,5.,5.] #(x0,y0,z0)
 t0=0.
 T=20.
 
-# avec la méthode para-real
-dt_G=0.1
-dt_F=0.01
+dt_G=0.01
+dt_F=0.001
 
-parareal_method(X0,t0,T,lorenz,dt_G,dt_F,gamma)
-
-# avec RK4
-dt = 0.01
-t = np.arange(t0,T+1e-9,dt)
-sol = RK4(X0,dt,t0,T,lorenz,gamma)
-
-sol_rk4 = panda.DataFrame(t,columns=['t'],dtype=np.float64)
-sol_rk4.insert(len(sol_rk4.columns),'x',sol[:,0])
-sol_rk4.insert(len(sol_rk4.columns),'y',sol[:,1])
-sol_rk4.insert(len(sol_rk4.columns),'z',sol[:,2])
-sol_rk4.to_csv('donnees_para_real/sol_rk4.csv')
+parareal_method(X0,t0,T,lorenz,dt_G,dt_F,gamma,True)

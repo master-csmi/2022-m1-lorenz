fa789ba0d4ca5ee07ac27996ca208fdd9509dc81
lecourtier.frederique <lecourtier.frederique@gmail.com>
2022-06-22 11:40:13 +0200

    doc file Make a documentation with doxygen #39

--- src/cpp/parareal/parareal.hpp
+++ src/cpp/parareal/parareal.hpp
@@ -1,7 +1,15 @@
 #include <Eigen/Dense>
 
-typedef Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor> Matrix;
-typedef Eigen::Matrix<double,1,Eigen::Dynamic> Vector;
+/**
+ * @file parareal.hpp
+ * @author Frédérique Lecourtier
+ * @brief 
+ * @version 1.0
+ * @date 2022-06-22
+ */
+
+using Matrix = Eigen::Matrix<double,Eigen::Dynamic,Eigen::Dynamic,Eigen::RowMajor>;
+using Vector = Eigen::Matrix<double,1,Eigen::Dynamic>;
 
 Matrix parareal(Vector X0_t0, double t0, double T, Vector prob(double, 
         Vector, int, double*), double dt_G, double dt_F, double* gamma, 
